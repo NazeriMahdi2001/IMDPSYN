f7d05f368a52244ece394b8441b43e9a426d6325
Thom Badings <tbadings@Thoms-MacBook-Pro.local>
2024-11-01 08:29:44 +0100

    create IMDP file (not integrated/adapted yet)

--- source/commons.py
+++ source/commons.py
@@ -6,3 +6,26 @@ def floor_decimal(a, precision=0):
     '''
 
     return np.round(a - 0.5 * 10 ** (-precision), precision)
+
+
+def writeFile(file, operation="w", content=[""]):
+    '''
+    Create a filehandle and store content in it.
+
+    Parameters
+    ----------
+    file : str
+        Filename to store the content in.
+    operation : str, optional
+        Type of operation to perform on the file. The default is "w".
+    content : list, optional
+        List of strings to store in the file. The default is [""].
+
+    Returns
+    -------
+    None.
+
+    '''
+    filehandle = open(file, operation)
+    filehandle.writelines(content)
+    filehandle.close()
