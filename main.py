07f0f8237b9b0ed8f30744f202c78777258d1e86
Mahdi Nazeri <nazerimahdi2001@gmail.com>
2024-11-14 15:10:34 +0000

    connect to prism - not checked

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from models.pendulum import InvertedPendulum
 from source.abstraction import Abstraction
 
+prism_executable='/root/IMDP/prism-4.8.1-linux64-x86/bin/prism'
+foldername='/root/IMDP/prism'
 
 abstraction = Abstraction(InvertedPendulum(), '/root/IMDP/models/pendulum.conf')
 abstraction.generate_noise_samples()
 abstraction.generate_samples()
 abstraction.find_actions()
 abstraction.find_transitions()
-abstraction.create_IMDP()
+abstraction.create_IMDP(foldername=foldername)
+abstraction.solve_iMDP(foldername=foldername, prism_executable=prism_executable)
